d6a1cef714ac3a6c6cd27e4f85bf16174c3d1945
marie <mcwimm@bitbucket.org>
2019-11-25 12:54:35 +0100

    [ProLib] added error messages

--- ProjectLib/XMLtoProject.py
+++ ProjectLib/XMLtoProject.py
@@ -15,34 +15,41 @@ from ProjectLib import Project
 ## Creates MangaProject defined in corresponding xml-file
 class XMLtoProject(Project.MangaProject):
     def __init__(self, **args):
-        self.args = {}
-        try:
-            self.prjfile = args["xml_project_file"]
-            self.readProjectFile()
-        except KeyError:
-            try:
-                tree = args["xml_tree"]
-                self.xmlTextStrip(tree)
-            except KeyError:
-                raise KeyError("Input not correct!")
-        self.addTreeDynamicConcepts()
-        self.addInitialPopulation()
-        self.addTreeTimeLoop()
+        from os import path  # required to run TestXMLToProject
 
-        self.argsToProject()
+        self.args = {}
+
+        if "xml_project_file" in args:
+            if not path.isfile(args["xml_project_file"]) or not path.exists(
+                    args["xml_project_file"]):
+                raise OSError('File is not a file or does not exist: ' +
+                              args["xml_project_file"])
+
+            elif not path.splitext(
+                    args["xml_project_file"])[-1].lower() == ".xml":
+                raise ValueError("File is not an xml file: " +
+                                 args["xml_project_file"])
+
+            else:
+                self.prjfile = args["xml_project_file"]
+                self.readProjectFile()
+                self.addTreeDynamicConcepts()
+                self.addInitialPopulation()
+                self.addTreeTimeLoop()
+
+                self.argsToProject()
+
+        elif "xml_tree" in args:
+            tree = args["xml_tree"]
 
     def readProjectFile(self):
         tree = etree.parse(self.prjfile)
         self.xmlTextStrip(tree)
 
     def xmlTextStrip(self, tree):
-        if tree.getroot() == None:
-            raise AttributeError("XML-Tree for project configuration is " +
-                                 "missing.")
         self.root = tree.getroot()
         #  The for loop removes ambiguous spaces of the tag arguments
         for tag in self.root.iter():
-            print(tag.text, tag)
             tag.text = tag.text.strip()
 
     def addTreeDynamicConcepts(self):
@@ -65,7 +72,7 @@ class XMLtoProject(Project.MangaProject):
     def findChild(self, parent, key):
         child = parent.find(key)
         if child is None:
-            raise KeyError("key '" + key + "' is missing in project file")
+            raise KeyError("Key '" + key + "' is missing in project file")
         return child
 
 
